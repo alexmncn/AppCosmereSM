26b593524dd0674132a338df1f878f5355f5e515
Adrián Benítez Roldán <149155965+AdrianBR003@users.noreply.github.com>
2024-10-30 13:02:33 +0100

    Fondo Completado y BottomNavView
    Falta añadir los libros para pincharlos en el mapa interactivo y añadir los botones en el bottom nav view

---
+++ app/src/main/java/com/example/appsandersonsm/MapaInteractivoActivity.kt.kt
@@ -0,0 +1,11 @@
+package com.example.appsandersonsm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapaInteractivoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mapa_interactivo) // Asegúrate de que el layout exista
+    }
+}
